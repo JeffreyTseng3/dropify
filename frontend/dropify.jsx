c0f826ba9f023ec74d221335c8147f33c685ad5e
jeffreytseng3 <jeffrey.tseng3@gmail.com>
2019-05-28 18:23:18 -0400

    make frontend action creators

--- frontend/dropify.jsx
+++ frontend/dropify.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { login } from "./util/session_api_util";
 
 document.addEventListener('DOMContentLoaded', () => {
+    window.login = login;
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Dropify</h1>, root);
 });

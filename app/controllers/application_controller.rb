db027c89943e9d2ec450fab5f539f53bdbc48c52
jeffreytseng3 <jeffrey.tseng3@gmail.com>
2019-05-28 15:02:37 -0400

    do CELLL in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,32 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    helper_method :current_user, :logged_in?
+
+    def current_user 
+        return nil unless session[:session_token]
+        @current_user ||= User.find_by_session_token(session[:session_token])
+    end 
+    
+    def logged_in?
+        !!current_user
+    end
+
+    def login!(user)
+        @current_user = user 
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout!
+        current_user.reset_session_token! 
+        session[:session_token] = nil 
+        @current_user = nil 
+    end
+
+    def ensure_logged_in 
+        # check the routes to see if need /api/
+        # redirect_to new_session_url unless logged_in? 
+    end
+
+
 end

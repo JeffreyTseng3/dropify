7a7703621b6eff3a1b9aef1083d342f14dc886a5
jeffreytseng3 <jeffrey.tseng3@gmail.com>
2019-06-02 17:05:39 -0400

    make playlists_on_explore_items files

---
+++ frontend/components/main/playlists_on_explore_item/playlists_on_explore_item.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+class PlaylistsOnExploreItem extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return ( 
+            <>
+                <h1>playlist item</h1>
+            </>
+        )
+    }
+
+}
+
+export default PlaylistsOnExploreItem;
---
+++ frontend/components/main/playlists_on_explore_item/playlists_on_explore_item_container.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { connect } from "react-redux";
+import PlaylistsOnExploreItem from "./playlists_on_explore_item";
+
+const msp = state => {
+    return {
+
+    }
+}
+
+const mdp = dispatch => {
+    return {
+
+    }
+}
+
+export default connect(msp, mdp)(PlaylistsOnExploreItem);
+

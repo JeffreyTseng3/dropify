f6a89a95cfa5a3827268519f1ddc2176ee026d19
jeffreytseng3 <jeffrey.tseng3@gmail.com>
2019-06-21 23:12:54 -0400

    update albums and ard artists reducers to receive playlist

--- frontend/reducers/albums_reducer.js
+++ frontend/reducers/albums_reducer.js
@@ -1,10 +1,11 @@
 import merge from 'lodash/merge';
 import { RECEIVE_ALBUMS, RECEIVE_ALBUM } from '../actions/album_actions';
 import { RECEIVE_ARTIST } from '../actions/artist_actions';
+import { RECEIVE_PLAYLIST } from '../actions/playlist_actions';
 
 const albumsReducer = (state = {}, action) => {
     Object.freeze(state);
-
+    let albums;
     switch(action.type) {
         case RECEIVE_ALBUMS:
             return merge({}, state, action.albums);
@@ -12,9 +13,11 @@ const albumsReducer = (state = {}, action) => {
             let album = action.payload.album;
             return merge({}, state, {[album.id]: album})
         case RECEIVE_ARTIST: 
-            let albums = action.payload.albums;
+            albums = action.payload.albums;
             return merge({}, state, albums);
-        
+        case RECEIVE_PLAYLIST: 
+            albums = action.payload.albums;
+            return merge({}, state, albums);
         default:
             return state;
     }
--- frontend/reducers/artists_reducer.js
+++ frontend/reducers/artists_reducer.js
@@ -1,10 +1,12 @@
 import merge from 'lodash/merge';
 import { RECEIVE_ARTISTS, RECEIVE_ARTIST } from "../actions/artist_actions";
 import {RECEIVE_ALBUM} from '../actions/album_actions';
+import { RECEIVE_PLAYLIST } from '../actions/playlist_actions';
 const artistsReducer = (state = {}, action) => {
     Object.freeze(state);
     
     let artist; 
+    let artists;
     switch(action.type) {
         case RECEIVE_ARTISTS:
             return merge({}, state, action.artists );
@@ -14,6 +16,10 @@ const artistsReducer = (state = {}, action) => {
         case RECEIVE_ALBUM: 
             artist = action.payload.artist;
             return merge({}, state, {[artist.id]: artist} )
+        case RECEIVE_PLAYLIST: 
+ 
+            artists = action.payload.artists;
+            return merge({}, state, artists)
         default: 
             return state;
     }
